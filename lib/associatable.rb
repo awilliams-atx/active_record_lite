a03d8f5d3e3a436c3640121c461c61f8b5dc9fde
Andrew Williams <andrew.williams.atx@gmail.com>
2016-06-26 16:37:57 -0400

    corrects bug in Associatable::has_one_through

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -28,42 +28,13 @@ module Associatable
   end
 
   def has_one_through(name, through_name, source_name)
-    source_options, through_table, source_table, source_foreign_key =
-      has_one_through_options(through_name, source_name)
+    through_options = self.assoc_options[through_name]
+    through_table = through_options.table_name
+    source_options = through_options.model_class.assoc_options[source_name]
+    source_table = source_options.table_name
+    source_foreign_key = source_options.foreign_key.to_s
 
-    query =
-      has_one_through_query(source_table, through_table, source_foreign_key)
-
-    define_association_method(query, name, source_options)
-  end
-
-  def assoc_options
-    @assoc_options ||= {}
-  end
-
-  private
-
-  def define_association_method(query, name, source_options)
-    define_method(:"#{name}") do
-      source_options.model_class
-        .new((DBConnection.execute(query, self.owner_id)).first)
-    end
-  end
-
-  def has_one_through_options(through_name, source_name)
-    source_options =
-      assoc_options[through_name].model_class.assoc_options[source_name]
-
-    [
-      source_options,
-      assoc_options[through_name].table_name,
-      source_options.table_name,
-      source_options.foreign_key.to_s
-    ]
-  end
-
-  def has_one_through_query(source_table, through_table, source_foreign_key)
-    <<-SQL
+    query = <<-SQL
       SELECT
         #{source_table}.*
       FROM
@@ -75,5 +46,17 @@ module Associatable
       WHERE
         #{through_table}.id = ?
     SQL
+
+    through_assoc = through_options.class_name.underscore.to_sym
+    
+    define_method(:"#{name}") do
+      return nil if self.send(through_assoc).nil?
+      source_options.model_class
+        .new((DBConnection.execute(query, self.send(through_assoc).id)).first)
+    end
+  end
+
+  def assoc_options
+    @assoc_options ||= {}
   end
 end
